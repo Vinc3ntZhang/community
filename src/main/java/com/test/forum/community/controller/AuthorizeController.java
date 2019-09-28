bb71f5523f776e138635309471dbab82b45c2047
JoshGGGG <1300442405@qq.com>
2019-09-28 21:20:17 +0800

    add check cookies

--- src/main/java/com/test/forum/community/controller/AuthorizeController.java
+++ src/main/java/com/test/forum/community/controller/AuthorizeController.java
@@ -11,7 +11,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
 @Controller
@@ -35,7 +37,8 @@ public class AuthorizeController {
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state,
-                           HttpServletRequest request) {
+                           HttpServletRequest request,
+                           HttpServletResponse response) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id("edb08beb3b762852a9b1");
         accessTokenDTO.setClient_secret("b572e1c24d177543c37ac67ac16d5dd7822f188c");
@@ -46,14 +49,17 @@ public class AuthorizeController {
         GitHubUser gitHubUser = gitHubProvider.getUser(accessToken);
         if (gitHubUser != null) {//登录成功，写cookie和session
             User user = new User();
-            user.setToken(UUID.randomUUID().toString());
+            String token = UUID.randomUUID().toString();
+            user.setToken(token);
             user.setName(gitHubUser.getName());
             user.setAccountId(String.valueOf(gitHubUser.getId()));
             user.setGmtCreate(System.currentTimeMillis());
             user.setGmtModified(user.getGmtCreate());
             userMapper.insert(user);
+            //添加Cookie
+            response.addCookie(new Cookie("token",token));
             //1、创建session
-            request.getSession().setAttribute("user", gitHubUser);
+//            request.getSession().setAttribute("user", gitHubUser);
             //2、重定向回index
             return "redirect:/";
         } else {//登录失败，重新登录
--- src/main/java/com/test/forum/community/controller/IndexController.java
+++ src/main/java/com/test/forum/community/controller/IndexController.java
@@ -1,12 +1,34 @@
 package com.test.forum.community.controller;
 
+import com.test.forum.community.mapper.UserMapper;
+import com.test.forum.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class IndexController {
+
+    //注入UserMapper
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/")
-    public String index() {
+    public String index(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals("token")) {
+                String token = cookie.getValue();
+                User user = userMapper.findByToken(token);
+                if (user != null) {
+                    request.getSession().setAttribute("user", user);
+                }
+                break;
+            }
+        }
         return "index";
     }
 }
