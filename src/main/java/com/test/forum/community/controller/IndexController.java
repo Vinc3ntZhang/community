003ce9a975fa37ecb18b200d6ec6cb24df5b2b61
JoshGGGG <1300442405@qq.com>
2019-09-30 19:18:30 +0800

    add QuestionDTO and QuestionService

--- src/main/java/com/test/forum/community/controller/IndexController.java
+++ src/main/java/com/test/forum/community/controller/IndexController.java
@@ -1,13 +1,17 @@
 package com.test.forum.community.controller;
 
+import com.test.forum.community.dto.QuestionDTO;
 import com.test.forum.community.mapper.UserMapper;
 import com.test.forum.community.model.User;
+import com.test.forum.community.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @Controller
 public class IndexController {
@@ -15,9 +19,14 @@ public class IndexController {
     //注入UserMapper
     @Autowired
     private UserMapper userMapper;
+    //注入QuestionMapper
+    @Autowired
+    private QuestionService questionService;
 
     @GetMapping("/")
-    public String index(HttpServletRequest request) {
+    public String index(HttpServletRequest request,
+                        Model model
+                        ) {
         Cookie[] cookies = request.getCookies();
         if (cookies !=null && cookies.length != 0){
             for (Cookie cookie : cookies) {
@@ -31,6 +40,8 @@ public class IndexController {
                 }
             }
         }
+        List<QuestionDTO> questionList =questionService.list();
+        model.addAttribute("question",questionList);
         return "index";
     }
 }
--- src/main/java/com/test/forum/community/mapper/QuestionMapper.java
+++ src/main/java/com/test/forum/community/mapper/QuestionMapper.java
@@ -3,9 +3,15 @@ package com.test.forum.community.mapper;
 import com.test.forum.community.model.Question;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 @Mapper
 public interface QuestionMapper {
     @Insert("insert into question (title,description,gmt_create,gmt_modified,creator,tag) values (#{title},#{description},#{gmtCreate},#{gmtModified},#{creator},#{tag})")
     void createQuestion(Question question);
+
+    @Select("select * from question")
+    List<Question> list();
 }
