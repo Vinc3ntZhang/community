8ddcffd809e1e0d4988a6cf0b93a7ee14e20d3c5
JoshGGGG <1300442405@qq.com>
2019-10-07 22:18:20 +0800

    Add problem details page

--- src/main/java/com/test/forum/community/controller/ProfileController.java
+++ src/main/java/com/test/forum/community/controller/ProfileController.java
@@ -11,9 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 
 @Controller
 public class ProfileController {
--- src/main/java/com/test/forum/community/service/QuestionService.java
+++ src/main/java/com/test/forum/community/service/QuestionService.java
@@ -95,4 +95,13 @@ public class QuestionService {
         paginationDTO.setPaginationQuestion(questionDTOList);
         return paginationDTO;
     }
+
+    public QuestionDTO getById(Integer id) {
+        Question question = questionMapper.getById(id);
+        QuestionDTO questionDTO = new QuestionDTO();
+        BeanUtils.copyProperties(question, questionDTO);
+        User user = userMapper.findById(question.getCreator());
+        questionDTO.setUser(user);
+        return questionDTO;
+    }
 }
