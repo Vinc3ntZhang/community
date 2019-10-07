b124e682e74d4033d1dc17a332e96e1f1690fe0f
JoshGGGG <1300442405@qq.com>
2019-10-07 15:23:14 +0800

    Add personal center and fixed some bugs

---
+++ src/main/java/com/test/forum/community/controller/ProfileController.java
@@ -0,0 +1,63 @@
+package com.test.forum.community.controller;
+
+import com.test.forum.community.dto.PaginationDTO;
+import com.test.forum.community.mapper.UserMapper;
+import com.test.forum.community.model.User;
+import com.test.forum.community.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class ProfileController {
+
+    @Autowired
+    private UserMapper userMapper;
+    @Autowired
+    private QuestionService questionService;
+
+    @GetMapping("/profile/{action}")
+    public String profile(@PathVariable(name = "action") String action,
+                          HttpServletRequest request,
+                          Model model,
+                          @RequestParam(name = "page", defaultValue = "1") Integer page,
+                          @RequestParam(name = "size", defaultValue = "2") Integer size
+    ) {
+        User user = null;
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null && cookies.length != 0) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("token")) {
+                    String token = cookie.getValue();
+                    user = userMapper.findByToken(token);
+                    if (user != null) {
+                        request.getSession().setAttribute("user", user);
+                    }
+                    break;
+                }
+            }
+        }
+
+        if (user == null) {
+            return "redirect:/";
+        }
+        if ("question".equals(action)) {
+            model.addAttribute("section", "question");
+            model.addAttribute("sectionName", "我的提问");
+        } else if ("replies".equals(action)) {
+            model.addAttribute("section", "replies");
+            model.addAttribute("sectionName", "最新回复");
+        }
+
+        PaginationDTO pagination = questionService.list(user.getId(), page, size);
+        model.addAttribute("pagination", pagination);
+        return "profile";
+    }
+
+}
